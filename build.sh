15361cb6500816f859e873767f635fd09eeb32e3
Martin Dullweber <martin@dul.li>
2023-02-19 12:06:57 +0100

    fix:  use local platform for initial image in build script

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 IMAGE=${1}
-PLATFORM=${2:-'linux/arm64,linux/amd64'}
+TARGET_PLATFORM=${2:-'linux/arm64,linux/amd64'}
 REPO=${REGISTRY:-'ghcr.io/dulli'}
 
 # First, build the image locally...
+local_platform=$(docker version --format '{{.Server.Os}}/{{.Server.Arch}}')
+echo "Building local image for: ${local_platform}"
+
 docker buildx build -f "images/${IMAGE}.dockerfile" \
-    --platform "linux/amd64" "images" \
+    --platform "${local_platform}" "images" \
     --tag "${IMAGE}:latest" --load
 
 # ... to be able to read the version file it contains ...
@@ -12,11 +15,11 @@ version=$(docker run --rm --entrypoint cat ${IMAGE}:latest /.version)
 major=`echo $version | cut -d. -f1`
 minor=`echo $version | cut -d. -f2`
 patch=`echo $version | cut -d. -f3`
-echo "${major}.${minor}.${patch}"
+echo "Service Version: ${major}.${minor}.${patch}"
 
 # ... and build/tag the image for all platforms.
 docker buildx build -f "images/${IMAGE}.dockerfile" \
-    --platform ${PLATFORM} "images" \
+    --platform ${TARGET_PLATFORM} "images" \
     --tag "${REPO}/${IMAGE}:latest" \
     --tag "${REPO}/${IMAGE}:${major}" \
     --tag "${REPO}/${IMAGE}:${major}.${minor}" \
